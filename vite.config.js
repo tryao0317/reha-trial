d60ec9df98afebf08a79ec444f19a16152d8d8ea
tryao0317 <t-aoki@juseikai.jp>
2025-10-09 20:37:20 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 export default defineConfig({
-  base: '/reha-trial/',   // ← ここを修正！
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@tensorflow-models/pose-detection',
+        '@tensorflow/tfjs-core',
+        '@tensorflow/tfjs-backend-webgl',
+        '@tensorflow/tfjs-backend-cpu'
+      ]
+    }
+  }
 })
-
