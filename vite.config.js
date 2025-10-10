595f9dcfc898b99fcf8e3168b303bd714a7ce5c3
tryao0317 <t-aoki@juseikai.jp>
2025-10-10 19:51:14 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHubリポジトリ名に合わせて修正（例: reha-trial）
 export default defineConfig({
-  base: '/reha-trial/',
+  base: '/reha-trial/', // ← GitHub Pagesのリポジトリ名に合わせる！
   plugins: [react()],
 })
